25a31b3db731c1301b7d53ff54fdd0bca7c1f91b
ar664 <ar664@njit.edu>
2026-05-04 15:13:08 -0400

    Add Genre Setter. Simplify skyscraper (Possible path bug on windows)

--- main.py
+++ main.py
@@ -42,6 +42,26 @@ def get_pis():
             continue
     return pi_hosts
 
+def request_pi(ip, port, data, endpoint):
+    url = 'http://' + ip + ':' + port + '/' + endpoint
+    headers = {"Content-Type": "application/json"}
+    #print(url)
+
+    encoded_data = json.dumps(data).encode("utf-8")
+    req = urllib.request.Request(url, data=encoded_data, headers=headers, method='POST')
+    try:
+        resp = urllib.request.urlopen(req).read()
+        body = json.loads(resp.decode("utf-8"))
+        print(body)
+        if(body['result']):
+            response = {'ip':pi, 'result': body['result']}
+        else:
+            response = {'ip':pi, 'result': 'unknown'}
+    except Exception as error:
+        response = {'ip':pi, 'result': error}
+        print(error)
+    return response
+
 def run_skyscraper(console):
     #ssCommand = '/opt/retropie/supplementary/skyscraper/Skyscraper'
     ssCommand = pathlib.Path.home().joinpath('Skyscraper/Skyscraper.exe')
@@ -54,11 +74,13 @@ def run_skyscraper(console):
     # -g /opt/retropie/configs/all/emulationstation/gamelists/psx 
     # -o /opt/retropie/configs/all/emulationstation/downloaded_media/psx 
 
+    input_path = pathlib.Path.home().joinpath('RetroPie/roms/')
     gamelists_path = pathlib.Path.home().joinpath('.emulationstation/gamelists/')
     dlmedia_path = pathlib.Path.home().joinpath('.emulationstation/downloaded_media/')
     options = ' --flags onlymissing,unattend,skipped'
 
-    command:str = '' + str(ssCommand) + ' -s screenscraper -p ' + console + ' -g ' + str(gamelists_path.joinpath(console)) + ' -o ' + str(dlmedia_path.joinpath(console)) + options
+    #command:str = '' + str(ssCommand) + ' -f emulationstation -s screenscraper -p ' + console + ' -i "' + str(input_path.joinpath(console)) + '" -g "' + str(gamelists_path.joinpath(console)) + '" -o "' + str(dlmedia_path.joinpath(console)) + '"' + options
+    command = str(ssCommand) + ' -p ' + console
     command = command.replace('\\', "\\\\")
     print(command)
 
@@ -73,8 +95,8 @@ def all_skyscraper(consoles):
 @st.cache_data
 def get_roms():
     roms = []
-    romDirs = []
     consoles = []
+    genres = {}
     extensions = []
 
     consoles = os.listdir(pathlib.Path.home().joinpath('.emulationstation/gamelists/'))
@@ -103,10 +125,14 @@ def get_roms():
                                 'name': game.find('name').text, 
                                 'genre': game.find('genre').text, 
                                 'players': game.find('players').text})
-    return consoles, roms
+                for genre in game.find('genre').text.split(','):
+                    genres[genre.strip()] = 1
+    genres = list(genres)
+    genres.sort()
+    return consoles, roms, genres
 
 
-consoles, roms = get_roms()
+consoles, roms, genres = get_roms()
 pis = get_pis()
 
 if len(consoles) > 0:
@@ -117,49 +143,53 @@ if len(consoles) > 0:
             with tabs[i]:
                 rom_table = [rom for rom in roms if rom['console'] == console]
                 st.dataframe(rom_table, height=400)
+    expander2 = st.expander("Genre List")
+    with expander2:
+        st.dataframe(genres, height=400)
 
 
 pi_col, rom_col = st.tabs(["Communicate", "File Management"])
 
 with pi_col:
-    st.header("Game Setter")
-
     pi_selection = st.pills("Select Pis", pis, selection_mode="multi")
 
+    st.header("Game Setter")
     console_selection =  st.selectbox("Select Console", consoles)
     if console_selection:
-        rom_selection = st.selectbox("Select Rom", [rom['name'] for rom in roms if rom['console'] == console_selection])
+        rom_sublist = [rom['name'] for rom in roms if rom['console'] == console_selection]
+        rom_sublist.sort()
+        rom_selection = st.selectbox("Select Rom", rom_sublist)
         if rom_selection and len(pi_selection) > 0:
             if st.button("Press to play on Pi(s)"):
                 print(console_selection + ' ' + rom_selection)
-                
-                headers = {"Content-Type": "application/json"}
                 data = {'rom': rom_selection, "console": console_selection}
+                
                 responses = []
                 for i, pi in enumerate(pi_selection):
-                    url = 'http://' + pi + ':5001/games'
-                    #print(url)
-                    encoded_data = json.dumps(data).encode("utf-8")
-                    req = urllib.request.Request(url, data=encoded_data, headers=headers, method='POST')
-                    try:
-                        resp = urllib.request.urlopen(req).read()
-                        body = json.loads(resp.decode("utf-8"))
-                        print(body)
-                        if(body['result']):
-                            responses.append({'ip':pi, 'result': body['result']})
-                        else:
-                            responses.append({'ip':pi, 'result': 'unknown'})
-                    except Exception as error:
-                        responses.append({'ip':pi, 'result': error})
-                        print(error)
-                        continue
+                    responses.append(request_pi(pi, '5001', data, 'games'))
+
                 st.table(responses)
+
+    st.header("Genre Setter")
+    genre_selection = st.selectbox("Select Genre", genres)
+    if genre_selection and len(pi_selection) > 0:
+        if st.button("Press to play on Pi(s)"):
+            print('Genre selection: ' + genre_selection)
+            data = {'genre': genre_selection}
+
+            responses = []
+            for i, pi in enumerate(pi_selection):
+                responses.append(request_pi(pi, '5001', data, 'random'))
+
+            st.table(responses)
+
+
 with rom_col:
     st.header("Rom Uploader")
 
     up_console =  st.selectbox("Select Console:", consoles, accept_new_options=True)
     up_roms = st.file_uploader("Upload Rom(s)", accept_multiple_files=True)
-    if up_roms is not None and len(up_console) > 0 and st.button("Process Rom"):
+    if up_roms is not None and len(up_console) > 0 and st.button("Process Rom(s)"):
         if not rom_directory.joinpath(up_console).exists():
             st.error(f"Rom directory {up_console} not found")
         else:
